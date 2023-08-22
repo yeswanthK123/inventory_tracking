07399e0df6bf9ccb6c46bdbc40ef4cef6518839f
yeswanthK123 <717821i260@kce.ac.in>
2023-08-22 09:23:49 +0530

    First Commit

---
+++ store/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+from .models import (
+    Supplier,
+    Buyer,
+    Drop,
+    Product,
+    Order,
+    Delivery
+)
+
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ['user', 'name', 'address', 'created_date']
+
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ['user', 'name', 'address', 'created_date']
+
+admin.site.register(Supplier, SupplierAdmin)
+admin.site.register(Buyer, BuyerAdmin)
+admin.site.register(Drop)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Delivery)
