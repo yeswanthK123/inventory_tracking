3e794bf0540cd8cb81b31ccf2f18e3b739a75596
yeswanthK123 <140743739+yeswanthK123@users.noreply.github.com>
2023-10-25 20:42:08 +0530

    Update settings.py

--- inventory/settings.py
+++ inventory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k2z9po4i#n+1p(^ny1el2c!om(^-l+_%&ob0azk0-ike*-)81e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['inventory-tracking-system.onrender.com']
 
 
 # Application definition
